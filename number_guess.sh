c59075f3b33540579e6e40f17a349a1e91f837f1
Krisdian Widya <krisdianwidya@gmail.com>
2024-05-08 08:17:31 +0000

    feat: create game logic functionality to read the guess input

--- number_guess.sh
+++ number_guess.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=number_guess -t -c"
 
+GAME_FUNCTION(){
+  # generate actual number
+  ACTUAL_NUM=$(( RANDOM % 1000 + 1 ))
+
+  # get guess number from input
+  if [[ $1 ]]
+  then
+    echo -e "\n$1"
+  else
+    echo -e "\nGuess the secret number between 1 and 1000:"
+  fi
+  read GUESS_NUM
+  
+  # check between number
+  if [[ ! $GUESS_NUM =~ ^[0-9]+$ ]]
+  then
+    GAME_FUNCTION "That is not an integer, guess again:"
+  fi
+}
+
 # print asking username
 echo "Enter your username:"
 read USERNAME
@@ -11,12 +31,12 @@ USERNAME_DB=$($PSQL "SELECT * FROM players WHERE username = '$USERNAME'");
 # check username in db
 if [[ -z $USERNAME_DB ]]
 then
-echo a
   # insert new data in db
   INSERT_PLAYER=$($PSQL "INSERT INTO players(username) VALUES('$USERNAME')")
   # print welcome msg for 1st time player
   echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
   # run main function
+  GAME_FUNCTION
 else
   # build formatted data
   echo $USERNAME_DB | while read PLAYER_ID BAR NAME BAR GAME_PLAYED BAR BEST_GAME
@@ -25,4 +45,6 @@ else
     echo -e "\nWelcome back, $NAME! You have played $GAME_PLAYED games, and your best game took $BEST_GAME guesses."
   done
   # run main function
+  GAME_FUNCTION
 fi
+
