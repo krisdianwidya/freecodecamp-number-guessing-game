302ec9ed651a2c7961006f1d1586e9370c7dac28
Krisdian Widya <krisdianwidya@gmail.com>
2024-05-08 04:44:02 +0000

    feat: add commented pseudocode

--- number_guess.sh
+++ number_guess.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 # print asking username
 echo "Enter your username:"
 read USERNAME
+
+# get username in db
+USERNAME_DB=$($PSQL "SELECT * FROM players WHERE username = '$USERNAME'");
+
+# check username in db
+if [[ -z $USERNAME_DB ]]
+then
+  # insert new data in db
+  # print welcome msg for 1st time player
+  # run main function
+else
+  # print welcome msg
+  # build formatted data
+  # print prev game played info
+  # run main function
+fi
