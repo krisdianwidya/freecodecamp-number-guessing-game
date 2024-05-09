890892eb4d1b66d02036a5b3d030dc8f924030fa
Krisdian Widya <krisdianwidya@gmail.com>
2024-05-09 09:19:03 +0000

    feat: develop game logic and refactor logic flow

--- number_guess.sh
+++ number_guess.sh
@@ -1,18 +1,48 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=number_guess -t -c"
 
+# generate actual number
+ACTUAL_NUM=$(( RANDOM % 1000 + 1 ))
+# set number guesses
+NUMBER_OF_GUESSES=0
+
+MAIN_FN(){
+  # print asking username
+  echo "Enter your username:"
+  read USERNAME
+
+  # get player id in db
+  PLAYER_ID=$($PSQL "SELECT player_id FROM players WHERE username = '$USERNAME'");
+
+  # check username in db
+  if [[ -z $PLAYER_ID ]]
+  then
+    # print welcome msg for 1st time player
+    echo "Welcome, $USERNAME! It looks like this is your first time here."
+    # insert new data in db
+    INSERT_PLAYER=$($PSQL "INSERT INTO players(username) VALUES('$USERNAME')")
+    # get player id in db
+    PLAYER_ID=$($PSQL "SELECT player_id FROM players WHERE username = '$USERNAME'");
+  else
+    USERNAME_DB=$($PSQL "SELECT username FROM players WHERE player_id = $PLAYER_ID" | sed 's/ //g' );
+    GAMES_PLAYED=$($PSQL "SELECT games_played FROM players WHERE player_id = $PLAYER_ID" | sed 's/ //g' );
+    BEST_GAME=$($PSQL "SELECT best_game FROM players WHERE player_id = $PLAYER_ID" | sed 's/ //g' );
+    
+    echo "Welcome back, $USERNAME_DB! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  fi
+
+  # run main function
+  GAME_FUNCTION
+}
+
 GAME_FUNCTION(){
-  # generate actual number
-  ACTUAL_NUM=$(( RANDOM % 1000 + 1 ))
-  echo actual num $ACTUAL_NUM
-  PLAYER_ID=$(echo $USERNAME_DB | grep -o '^.')
   
   # get guess number from input
   if [[ $1 ]]
   then
-    echo -e "\n$1"
+    echo "$1"
   else
-    echo -e "\nGuess the secret number between 1 and 1000:"
+    echo "Guess the secret number between 1 and 1000:"
   fi
   read GUESS_NUM
   
@@ -20,50 +50,34 @@ GAME_FUNCTION(){
   if [[ ! $GUESS_NUM =~ ^[0-9]+$ ]]
   then
     GAME_FUNCTION "That is not an integer, guess again:"
-  else
+  else    
+    NUMBER_OF_GUESSES=$(( $NUMBER_OF_GUESSES + 1 ))
+
     # the input is number, check if the numbers is same
     if [[ $ACTUAL_NUM = $GUESS_NUM ]]
     then
-      echo ok
+      # print message
+      echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $GUESS_NUM. Nice job!"
+
+      # add best_game if it is not 0 and few than prev best_game
+      PREV_BEST_GAME=$($PSQL "SELECT best_game FROM players WHERE player_id = $PLAYER_ID")
+
+      if [[ $PREV_BEST_GAME -eq 0 || $NUMBER_OF_GUESSES -lt $PREV_BEST_GAME ]]
+      then
+        UPDATE_GAMES_PLAYED=$($PSQL "UPDATE players SET games_played = games_played + $NUMBER_OF_GUESSES WHERE player_id = $PLAYER_ID")
+        UPDATE_BEST_GAME=$($PSQL "UPDATE players SET best_game = $NUMBER_OF_GUESSES WHERE player_id = $PLAYER_ID")
+      fi
     else
-      # add games_played
-      UPDATE_GAMES_PLAYED=$($PSQL "UPDATE players SET games_played = games_played + 1 WHERE player_id = $PLAYER_ID")
-      
       # check if lower or higher
-      if [[ $ACTUAL_NUM > $GUESS_NUM ]]
+      if [[ $GUESS_NUM -gt $ACTUAL_NUM ]]
       then
         GAME_FUNCTION "It's lower than that, guess again:"
-      else
+      elif [[ $GUESS_NUM -lt $ACTUAL_NUM ]]
+      then
         GAME_FUNCTION "It's higher than that, guess again:"
       fi
     fi
   fi
 }
 
-# print asking username
-echo "Enter your username:"
-read USERNAME
-
-# get username in db
-USERNAME_DB=$($PSQL "SELECT * FROM players WHERE username = '$USERNAME'");
-
-# check username in db
-if [[ -z $USERNAME_DB ]]
-then
-  # insert new data in db
-  INSERT_PLAYER=$($PSQL "INSERT INTO players(username) VALUES('$USERNAME')")
-  # print welcome msg for 1st time player
-  echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
-  # run main function
-  GAME_FUNCTION
-else
-  # build formatted data
-  echo $USERNAME_DB | while read PLAYER_ID BAR NAME BAR GAME_PLAYED BAR BEST_GAME
-  do
-    # print prev game played info
-    echo -e "\nWelcome back, $NAME! You have played $GAME_PLAYED games, and your best game took $BEST_GAME guesses."
-  done
-  # run main function
-  GAME_FUNCTION
-fi
-
+MAIN_FN
