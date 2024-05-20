17013735c6f4ab0f7b46edfef0ab4d585f777697
Krisdian Widya <krisdianwidya@gmail.com>
2024-05-20 05:44:29 +0000

    fix: update logic of updating data for games played and best game field

--- number_guess.sh
+++ number_guess.sh
@@ -23,6 +23,8 @@ MAIN_FN(){
     INSERT_PLAYER=$($PSQL "INSERT INTO players(username) VALUES('$USERNAME')")
     # get player id in db
     PLAYER_ID=$($PSQL "SELECT player_id FROM players WHERE username = '$USERNAME'");
+    GAMES_PLAYED=0
+    BEST_GAME=0
   else
     USERNAME_DB=$($PSQL "SELECT username FROM players WHERE player_id = $PLAYER_ID" | sed 's/ //g' );
     GAMES_PLAYED=$($PSQL "SELECT games_played FROM players WHERE player_id = $PLAYER_ID" | sed 's/ //g' );
@@ -36,7 +38,7 @@ MAIN_FN(){
 }
 
 GAME_FUNCTION(){
-  
+  NUMBER_OF_GUESSES=$(( $NUMBER_OF_GUESSES + 1 ))
   # get guess number from input
   if [[ $1 ]]
   then
@@ -50,22 +52,20 @@ GAME_FUNCTION(){
   if [[ ! $GUESS_NUM =~ ^[0-9]+$ ]]
   then
     GAME_FUNCTION "That is not an integer, guess again:"
-  else    
-    NUMBER_OF_GUESSES=$(( $NUMBER_OF_GUESSES + 1 ))
-
+  else
     # the input is number, check if the numbers is same
     if [[ $ACTUAL_NUM = $GUESS_NUM ]]
     then
       # print message
-      echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $GUESS_NUM. Nice job!"
+      echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $ACTUAL_NUM. Nice job!"
 
       # add best_game if it is not 0 and few than prev best_game
       PREV_BEST_GAME=$($PSQL "SELECT best_game FROM players WHERE player_id = $PLAYER_ID")
 
       if [[ $PREV_BEST_GAME -eq 0 || $NUMBER_OF_GUESSES -lt $PREV_BEST_GAME ]]
       then
-        UPDATE_GAMES_PLAYED=$($PSQL "UPDATE players SET games_played = games_played + $NUMBER_OF_GUESSES WHERE player_id = $PLAYER_ID")
-        UPDATE_BEST_GAME=$($PSQL "UPDATE players SET best_game = $NUMBER_OF_GUESSES WHERE player_id = $PLAYER_ID")
+        GAMES_PLAYED=$((GAMES_PLAYED+1))
+        UPDATE_RESULTS=$($PSQL "UPDATE players SET best_game = $NUMBER_OF_GUESSES, games_played = $GAMES_PLAYED WHERE player_id = $PLAYER_ID")
       fi
     else
       # check if lower or higher
